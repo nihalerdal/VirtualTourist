313e169cc66010dfd312d1841eab57ae04a0d568
nihalerdal <nihalerdall@gmail.com>
2021-09-10 18:34:12 +0300

    create UI

---
+++ VirtualTourist/View Controllers/PhotoAlbumVC.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoAlbumVC.swift
+//  VirtualTourist
+//
+//  Created by Nihal Erdal on 9/10/21.
+//
+
+import UIKit
+import MapKit
+
+class PhotoAlbumVC: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VirtualTourist/View Controllers/TravelLocationMapVC.swift
@@ -0,0 +1,35 @@
+//
+//  TravelLocationMapVC.swift
+//  VirtualTourist
+//
+//  Created by Nihal Erdal on 9/10/21.
+//
+
+import UIKit
+import MapKit
+
+
+
+class TravelLocationMapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
