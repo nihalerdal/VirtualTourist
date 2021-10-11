d8fead07a7a9c6a00fe8e0c6d104be460418ce16
nihalerdal <nihalerdall@gmail.com>
2021-10-11 16:27:05 -0700

    configure appDelegate to use coredata stack

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    
+    let dataController = DataController(modelName: "VirtualTourist")
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        dataController.load()
+         
         return true
     }
 
