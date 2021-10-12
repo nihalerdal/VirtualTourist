cdd87d6bd977c79ba9639d374bbe06671d0300e2
nihalerdal <nihalerdall@gmail.com>
2021-10-11 18:36:44 -0700

    drop pin by long press gesture

--- VirtualTourist/View Controllers/TravelLocationMapVC.swift
+++ VirtualTourist/View Controllers/TravelLocationMapVC.swift
@@ -9,17 +9,39 @@ import UIKit
 import MapKit
 
 
-
 class TravelLocationMapVC: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
-    
+    var dataController : DataController?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         mapView.delegate = self
+        mapView.isUserInteractionEnabled = true
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longTap(gesture: )))
+        mapView.addGestureRecognizer(gestureRecognizer)
+   
+    }
+    
+    @objc func longTap(gesture: UILongPressGestureRecognizer ){
+        //as soon as  long press is ended
+        if gesture.state == .ended{
+            //get coordinates and create the pin
+            let location = gesture.location(in: mapView)
+            let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            mapView.addAnnotation(annotation)
+            
+        }
+    }
+    
     
     /*
     // MARK: - Navigation
